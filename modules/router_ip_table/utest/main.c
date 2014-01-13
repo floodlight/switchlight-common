1297db7aa333f3eec3af3c38914163a6eb7cd784
Rich Lane <rlane@bigswitch.com>
2014-01-13 13:38:25 -0800

    router_ip_table: skeleton gentable ops
    Validate key and value but don't do anything with it.

--- modules/router_ip_table/utest/main.c
+++ modules/router_ip_table/utest/main.c
@@ -10,10 +10,104 @@
 #include <string.h>
 #include <AIM/aim.h>
 
+#include <router_ip_table/router_ip_table.h>
+#include <indigo/of_state_manager.h>
+
+static const indigo_core_gentable_ops_t *ops;
+static void *table_priv;
+
+static of_list_bsn_tlv_t *
+make_key(uint16_t vlan_vid)
+{
+    of_list_bsn_tlv_t *list = of_list_bsn_tlv_new(OF_VERSION_1_3);
+    of_bsn_tlv_vlan_vid_t *tlv = of_bsn_tlv_vlan_vid_new(OF_VERSION_1_3);
+    of_bsn_tlv_vlan_vid_value_set(tlv, vlan_vid);
+    of_list_append(list, tlv);
+    of_object_delete(tlv);
+    return list;
+}
+
+static of_list_bsn_tlv_t *
+make_value(uint32_t ipv4)
+{
+    of_list_bsn_tlv_t *list = of_list_bsn_tlv_new(OF_VERSION_1_3);
+    of_bsn_tlv_ipv4_t *tlv = of_bsn_tlv_ipv4_new(OF_VERSION_1_3);
+    of_bsn_tlv_ipv4_value_set(tlv, ipv4);
+    of_list_append(list, tlv);
+    of_object_delete(tlv);
+    return list;
+}
+
 int aim_main(int argc, char* argv[])
 {
-    printf("router_ip_table Utest Is Empty\n");
-    router_ip_table_config_show(&aim_pvs_stdout);
+    of_list_bsn_tlv_t *key1, *key2, *value1, *value2, *value3;
+    void *entry_priv;
+    indigo_error_t rv;
+
+    router_ip_table_init();
+
+    ASSERT(ops != NULL);
+
+    key1 = make_key(10);
+    key2 = make_key(8000); /* invalid */
+    value1 = make_value(0x1234);
+    value2 = make_value(0x5678);
+    value3 = make_value(0); /* invalid */
+
+    /* Successful add/modify/delete */
+    {
+        rv = ops->add(table_priv, key1, value1, &entry_priv);
+        ASSERT(rv == INDIGO_ERROR_NONE);
+
+        rv = ops->modify(table_priv, entry_priv, key1, value2);
+        ASSERT(rv == INDIGO_ERROR_NONE);
+
+        rv = ops->del(table_priv, entry_priv, key1);
+        ASSERT(rv == INDIGO_ERROR_NONE);
+    }
+
+    /* Invalid key */
+    {
+        rv = ops->add(table_priv, key2, value1, &entry_priv);
+        ASSERT(rv == INDIGO_ERROR_PARAM);
+    }
+
+    /* Invalid value */
+    {
+        rv = ops->add(table_priv, key1, value3, &entry_priv);
+        ASSERT(rv == INDIGO_ERROR_PARAM);
+    }
+
+    of_object_delete(key1);
+    of_object_delete(key2);
+    of_object_delete(value1);
+    of_object_delete(value2);
+    of_object_delete(value3);
+
+    router_ip_table_finish();
+
     return 0;
 }
 
+void
+indigo_core_gentable_register(
+    const of_table_name_t name,
+    const indigo_core_gentable_ops_t *_ops,
+    void *_table_priv,
+    uint32_t max_size,
+    uint32_t buckets_size,
+    indigo_core_gentable_t **gentable)
+{
+    if (!strcmp(name, "router_ip")) {
+        ops = _ops;
+        table_priv = _table_priv;
+    }
+
+    *gentable = (void *)1;
+}
+
+void
+indigo_core_gentable_unregister(indigo_core_gentable_t *gentable)
+{
+    ASSERT(gentable == (void *)1);
+}
