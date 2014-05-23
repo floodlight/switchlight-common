4daf76a8de1096cacc463985334907c98271fe25
Harshmeet Singh <harshmeet.singh@bigswitch.com>
2014-05-23 13:26:46 -0700

    untagged packet support and minor ucli additions

--- modules/arpra/module/src/arpra_ucli.c
+++ modules/arpra/module/src/arpra_ucli.c
@@ -13,6 +13,22 @@
 #include <uCli/ucli_handler_macros.h>
 
 static ucli_status_t
+arpra_ucli_ucli__clear_counters__(ucli_context_t* uc)
+{
+    UCLI_COMMAND_INFO(uc,
+                      "clear", 0,
+                      "$summary#Clear packet counters.");
+
+    if (!arpra_is_initialized()) return UCLI_STATUS_E_ERROR;
+
+    debug_counter_reset(&pkt_counters.total_in_packets);
+    debug_counter_reset(&pkt_counters.total_out_packets);
+    debug_counter_reset(&pkt_counters.internal_errors);
+    
+    return UCLI_STATUS_OK;
+}
+
+static ucli_status_t
 arpra_ucli_ucli__show_counters__(ucli_context_t* uc)
 {
     UCLI_COMMAND_INFO(uc,
@@ -47,12 +63,13 @@ arpra_ucli_ucli__show_cache__(ucli_context_t* uc)
 
     if (list_empty(cache)) return UCLI_STATUS_OK;
 
-    ucli_printf(uc, "IP --> MAC\n");
+    ucli_printf(uc, "IP --> MAC\t\t\t\t Refcount\n");
     LIST_FOREACH(cache, cur) {
         arp_cache_entry_t *cache_entry = container_of(cur, links,
                                                       arp_cache_entry_t);
-        ucli_printf(uc, "%{ipv4a} --> %{mac}\n", 
-                    cache_entry->entry.ipv4, cache_entry->entry.mac.addr); 
+        ucli_printf(uc, "%{ipv4a} --> %{mac}\t %d\n", 
+                    cache_entry->entry.ipv4, cache_entry->entry.mac.addr,
+                    cache_entry->refcount); 
     }
 
     return UCLI_STATUS_OK;
@@ -73,6 +90,7 @@ arpra_ucli_ucli__config__(ucli_context_t* uc)
  *****************************************************************************/
 static ucli_command_handler_f arpra_ucli_ucli_handlers__[] = 
 {
+    arpra_ucli_ucli__clear_counters__,
     arpra_ucli_ucli__show_counters__,
     arpra_ucli_ucli__show_cache__,
     arpra_ucli_ucli__config__,
